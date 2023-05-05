2cf6ee360d768862e7da9040c35ef0bce45f55b3
prints-philbert <119360081+prints-philbert@users.noreply.github.com>
2023-05-04 19:42:02 -0500

    Rendering the Add User Form

---
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -0,0 +1,22 @@
+package org.launchcode.spaday.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("user")
+public class UserController {
+
+
+
+
+    @GetMapping("add")
+    public String displayAddUserForm() {
+        return "user/add";
+    }
+
+}
